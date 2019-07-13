5a09a7d18783125400e34d67780a22e2c70b9cf8
Puleeno Nguyen <puleeno@gmail.com>
2019-07-14 06:46:53 +0700

    Update structure for Jankx

--- src/JankxFields.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Jankx\Options;
-
-use Jankx\Abstracts\OptionFramework;
-
-class JankxFields extends OptionFramework
-{
-}
--- src/Redux.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Jankx\Options;
-
-use Jankx\Abstracts\OptionFramework;
-
-class Redux extends OptionFramework
-{
-}
