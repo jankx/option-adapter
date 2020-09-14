a452121e1907d86fa002e6ac455c12d68dc8500b
Puleeno Nguyen <puleeno@gmail.com>
2020-09-14 20:18:08 +0700

    Move get option to new class to can be extend by other themes

--- helpers.php
+++ helpers.php
@@ -1,31 +1,10 @@
 <?php
 
-use Jankx\Option\Framework;
-use Jankx\Option\OptionFrameworkAdapter;
+use Jankx\Option\Option;
 
 if (!function_exists('jankx_get_option')) {
     function jankx_get_option($optionName, $defaultValue = null)
     {
-        $pre = apply_filters("jankx_get_option_{$optionName}", null, $defaultValue);
-        if (!is_null($pre)) {
-            return $pre;
-        }
-
-        $framework = Framework::getFramework();
-        if (is_null($framework)) {
-            return $defaultValue;
-        }
-
-        if (!is_a($framework, OptionFrameworkAdapter::class)) {
-            throw new \Exception(sprintf(
-                'The option framework must be an instance of %s',
-                OptionFrameworkAdapter::class
-            ));
-        }
-
-        return $framework->getOption(
-            $defaultValue,
-            $defaultValue
-        );
+        return Option::get($optionName, $defaultValue);
     }
 }
---
+++ src/Option.php
@@ -0,0 +1,33 @@
+<?php
+namespace Jankx\Option;
+
+use Jankx\Option\Framework;
+use Jankx\Option\OptionFrameworkAdapter;
+
+class Option
+{
+    public static function get($optionName, $defaultValue = null)
+    {
+        $pre = apply_filters("jankx_get_option_{$optionName}", null, $defaultValue);
+        if (!is_null($pre)) {
+            return $pre;
+        }
+
+        $framework = Framework::getFramework();
+        if (is_null($framework)) {
+            return $defaultValue;
+        }
+
+        if (!is_a($framework, OptionFrameworkAdapter::class)) {
+            throw new \Exception(sprintf(
+                'The option framework must be an instance of %s',
+                OptionFrameworkAdapter::class
+            ));
+        }
+
+        return $framework->getOption(
+            $defaultValue,
+            $defaultValue
+        );
+    }
+}
