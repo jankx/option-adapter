b1e1dcfad546a098701ee06b780a9238886c9cd6
Puleeno Nguyen <puleeno@gmail.com>
2025-01-06 15:24:02 +0700

    try define options via new option framework

--- src/Frameworks/JankxOptionFramework.php
+++ src/Frameworks/JankxOptionFramework.php
@@ -4,18 +4,19 @@ namespace Jankx\Adapter\Options\Frameworks;
 
 use Jankx\Adapter\Options\Abstracts\Adapter;
 use Jankx\Adapter\Options\Specs\Options;
+
+use Jankx\Dashboard\Elements\Field;
+use Jankx\Dashboard\Elements\Section;
+use Jankx\Dashboard\Elements\Page;
 use Jankx\Dashboard\OptionFramework;
+
 use Jankx\GlobalConfigs;
 
 class JankxOptionFramework extends Adapter
 {
-    public function setArgs($args)
-    {
-    }
+    public function setArgs($args) {}
 
-    public function addSection($section)
-    {
-    }
+    public function addSection($section) {}
 
     public static function mapSectionFields()
     {
@@ -27,9 +28,7 @@ class JankxOptionFramework extends Adapter
         return [];
     }
 
-    public function getOption($name, $defaultValue = null)
-    {
-    }
+    public function getOption($name, $defaultValue = null) {}
 
     public function register_admin_menu($menu_title, $display_name)
     {
@@ -39,11 +38,44 @@ class JankxOptionFramework extends Adapter
             $themeInfo->get('Name')
         ) : $themeInfo->get('Name');
 
-        new OptionFramework(
+        // Tạo instance của OptionFramework
+        $optionsFramework = new OptionFramework(
             'jankx_options',
             $display_name,
             $menu_title,
         );
+
+        // Tạo page Cài Đặt Chung
+        $generalSettingsPage = new Page('Cài Đặt Chung');
+        $generalSettingsSection = new Section('Cài Đặt Chung');
+        $generalSettingsSection->addField(new Field('site_logo', 'Logo của Trang', 'input'));
+        $generalSettingsSection->addField(new Field('site_description', 'Mô Tả Trang', 'textarea'));
+        $generalSettingsPage->addSection($generalSettingsSection);
+        $optionsFramework->addPage($generalSettingsPage);
+
+        // Tạo page Cài Đặt Màu Sắc
+        $colorSettingsPage = new Page('Cài Đặt Màu Sắc');
+        $colorSettingsSection = new Section('Cài Đặt Màu Sắc');
+        $colorSettingsSection->addField(new Field('color_scheme', 'Màu Sắc', 'select', [
+            'options' => [
+                'light' => 'Sáng',
+                'dark' => 'Tối'
+            ]
+        ]));
+        $colorSettingsPage->addSection($colorSettingsSection);
+        $optionsFramework->addPage($colorSettingsPage);
+
+        // Tạo page Cài Đặt Tính Năng
+        $featureSettingsPage = new Page('Cài Đặt Tính Năng');
+        $featureSettingsSection = new Section('Cài Đặt Tính Năng');
+        $featureSettingsSection->addField(new Field('enable_feature_x', 'Kích Hoạt Tính Năng X', 'select', [
+            'options' => [
+                'yes' => 'Có',
+                'no' => 'Không'
+            ]
+        ]));
+        $featureSettingsPage->addSection($featureSettingsSection);
+        $optionsFramework->addPage($featureSettingsPage);
     }
 
     public function createSections($options)
